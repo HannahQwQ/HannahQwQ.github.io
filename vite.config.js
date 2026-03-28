f2a55a40ff19ec72fd439e789dc2b5ad9b4a1812
HannahQwQ <qzsetdhjiok2@163.com>
2026-03-28 19:14:57 +0800

    add base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 1. 引入 path 模块
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
+  resolve: {
+    alias: {
+      // 2. 定义别名：将 "@" 映射到项目根目录下的 "src" 目录
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
